af69ee5a82c75f1536a99de709dea84b7d8c5fa1
Hamachi-300 <noname14463@gmail.com>
2026-08-27 23:39:05 +0700

    just set boss hp back

--- Assets/Scripts/Boss/BossController.cs
+++ Assets/Scripts/Boss/BossController.cs
@@ -55,7 +55,7 @@ public class BossController : MonoBehaviour
     private float sleepTime = 3f;
     private float visibleRange = 10f;
 
-    private float maxHp = 3f;
+    private float maxHp = 300f;
     private float hp = 0;
 
     private bool isDie = false;
